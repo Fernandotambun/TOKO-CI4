2e349dd8436bd4b118b12c10998e40793a78c84c
Fernandotambun <111202214723@mhs.dinus.ac.id>
2023-07-03 01:35:07 +0700

    first commit

--- pwl_4219.sql
+++ pwl_4219.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2023 at 03:43 PM
+-- Generation Time: Jul 02, 2023 at 07:20 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
